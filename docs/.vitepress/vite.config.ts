1f1b6f7a1a34eb267cb877ba0aae388256551349
Nick Schaap <schaap@adobe.com>
2025-07-24 13:47:05 -0400

    updates

--- docs/.vitepress/vite.config.ts
+++ docs/.vitepress/vite.config.ts
@@ -27,18 +27,6 @@ export default defineConfig({
     {
       name: 'sample-app-lit-e2e',
       configureServer(server) {
-        if (process.env.E2E_TEST && !process.env.CI) {
-          const TIMEOUT_DURATION = 2_000; // 2 seconds
-
-          // Start a one-time timeout when server is configured
-          setTimeout(() => {
-            console.log(
-              'No requests received for 2 seconds after startup, shutting down server...',
-            );
-            server.close();
-          }, TIMEOUT_DURATION);
-        }
-
         server.middlewares.use((req, res, next) => {
           if (process.env.E2E_TEST && req.url?.includes('/e2e-done')) {
             res.statusCode = 200;
@@ -57,6 +45,6 @@ export default defineConfig({
   ],
 
   server: {
-    open: !Boolean(process.env.E2E_TEST),
+    open: !Boolean(process.env.CI) && !Boolean(process.env.E2E_TEST),
   },
 });
