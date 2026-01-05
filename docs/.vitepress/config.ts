daf833f2c6c86d53fafff583a0557444394611a8
Shane Daniel <135388+simshanith@users.noreply.github.com>
2026-01-04 20:19:59 -0800

    docs: add Open Graph and Twitter Card meta tags (#80)
    * docs: add Open Graph and Twitter Card meta tags
    
    * lint: remove unused import
    
    * docs: prefix page title with site title

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig, HeadConfig } from 'vitepress';
 
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
+const baseUrl = 'https://lit-ui-router.dev';
+
+const config = {
   outDir: 'dist',
   title: 'Lit UI Router',
   description: 'A @uirouter implementation for Lit',
@@ -9,6 +10,60 @@ export default defineConfig({
   ignoreDeadLinks: ['/app'],
   vite: {
     configFile: './.vitepress/vite.config.ts',
+  },
+};
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  ...config,
+  transformHead({ pageData }) {
+    const head: HeadConfig[] = [];
+    const imageUrl = `${baseUrl}/images/lit-ui-router.svg`;
+    const imageAlt = 'Lit UI Router logo';
+    const twitterCreator = '@simloovoo';
+    const twitterCard = 'summary_large_image';
+    const ogType = 'website';
+
+    let title: string;
+    let description: string;
+    let pageUrl: string;
+
+    if (pageData.relativePath === 'index.md') {
+      title = config.title;
+      description = config.description;
+      pageUrl = baseUrl;
+    } else {
+      title = pageData.frontmatter.title || pageData.title;
+      title = title ? `${config.title} - ${title}` : config.title;
+
+      description =
+        pageData.frontmatter.description ||
+        pageData.description ||
+        config.description;
+
+      const pagePath = pageData.relativePath
+        .replace(/^\.\//, '')
+        .replace(/\/index\.md$/, '/')
+        .replace(/\.md$/, '');
+      pageUrl = `${baseUrl}/${pagePath}`;
+    }
+
+    head.push(
+      ['meta', { property: 'og:title', content: title }],
+      ['meta', { property: 'og:description', content: description }],
+      ['meta', { property: 'og:image', content: imageUrl }],
+      ['meta', { property: 'og:image:alt', content: imageAlt }],
+      ['meta', { property: 'og:url', content: pageUrl }],
+      ['meta', { property: 'og:type', content: ogType }],
+      ['meta', { property: 'og:site_name', content: config.title }],
+      ['meta', { name: 'twitter:card', content: twitterCard }],
+      ['meta', { name: 'twitter:creator', content: twitterCreator }],
+      ['meta', { name: 'twitter:title', content: title }],
+      ['meta', { name: 'twitter:description', content: description }],
+      ['meta', { name: 'twitter:image', content: imageUrl }],
+      ['meta', { name: 'twitter:image:alt', content: imageAlt }],
+    );
+    return head;
   },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
