d72bf51983e8de0fd5088092b84df1f5f4302d7f
Shane Daniel <simrpgman@gmail.com>
2026-01-05 21:52:13 -0800

    lint: turbo format

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,8 @@ import './custom.css';
 
 export default DefaultTheme;
 
-export const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+export const isChrome =
+  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 if (isChrome) {
-  document.documentElement.classList.add('chrome')
+  document.documentElement.classList.add('chrome');
 }
