ab316be6d15c3db55f91a109a3a11b78ef7e1cb4
Shane Daniel <simrpgman@gmail.com>
2026-01-05 21:24:28 -0800

    docs: mix-blend-mode for chrome only

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,3 +2,8 @@ import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 
 export default DefaultTheme;
+
+export const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (isChrome) {
+  document.documentElement.classList.add('chrome')
+}
