acd2756ef827f943d332360646ac42adbe97b612
Shane Daniel <135388+simshanith@users.noreply.github.com>
2026-01-05 23:18:51 -0800

    docs: homepage hero mix-blend-mode (#86)
    * docs: homepage hero mix-blend-mode
    
    * docs: switch to brightness % for safari maybe?
    
    * docs: mix-blend-mode for chrome only
    
    * docs: switch dark blend mode to plus-lighter
    
    * lint: turbo format

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,3 +2,9 @@ import DefaultTheme from 'vitepress/theme';
 import './custom.css';
 
 export default DefaultTheme;
+
+export const isChrome =
+  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (isChrome) {
+  document.documentElement.classList.add('chrome');
+}
